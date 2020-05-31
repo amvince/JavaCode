42a2cc2197239ed959db574c4461d2fd83b598d4
Ash Vince <amvince@gmail.com>
2020-05-31 12:37:38 -0700

    Create SieveOfEratosthenes.java

---
+++ Arrays/SieveOfEratosthenes.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+//Prime numbers using the Sieve of Eratosthenes method
+
+public class SieveOfEratosthenes {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n;
+		
+		System.out.println("What is the maximum value of primes you want to list?");
+		n = sc.nextInt();
+		
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		
+		// 1. Create an ArrayList containing all number from 2 to n
+		for (int i = 2; i<=n; i++) {
+			primes.add(i);
+		}
+		
+		// 2. Start with p = 2, the first prime number
+		int p = 2;
+		
+		while (p<n) {
+			// 3. remove all elements of the list that are multiples of p
+			for (int i = p*p; i<n; i+=p) {
+				if (primes.contains(i)) primes.remove(primes.indexOf(i));	
+			}
+			// 4. Increment p to the next value in the ArrayList - this will be the next-lowest prime.
+			p = primes.get(primes.indexOf(p)+1);
+		}
+		// 5. The while loop continues until p is greater than n
+		
+		// 6. The "primes" ArrayList now contains only primes.
+
+		// Print out the list.
+		System.out.println(primes);
+	}
+}
